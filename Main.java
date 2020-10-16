6f83e8977cc1591f6419640316d468f6448c4e70
dcurtis14 <donald.curtis@patelhs.org>
2020-10-16 04:48:04 +0000

    i added closing brackets

--- Main.java
+++ Main.java
@@ -6,12 +6,15 @@
   {
     System.out.println("There are 4 ants.");
   }else{
-    System.out.println("There are not 4 ants.");
-Scanner scan = new Scanner (System.in);
+      System.out.println("There are not 4 ants.");
+      Scanner scan = new Scanner (System.in);
 
-System.out.println("My question about ant hill");
-String question = scan.next();
-question += Scan.nextline();
+      System.out.println("My question about ant hill");
+      String question = scan.next();
+          question += Scan.nextline();
 
-System.out.println("Hey"+ question);
+          System.out.println("Hey"+ question);
 
+        }
+      }
+   }
