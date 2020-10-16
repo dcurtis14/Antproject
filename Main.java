22f3f4941390fc909913bcb09f8bafdbe24d5353
dcurtis14 <donald.curtis@patelhs.org>
2020-10-16 03:53:59 +0000

    I added a if else statment to my code

--- Main.java
+++ Main.java
@@ -1,3 +1,11 @@
-public class Main {
+   class Antproject {
     public static void main(String[] args) {
-        int a =2
+  int ants = 4;
+
+  if(ants == 4)
+  {
+    System.out.println("There are 4 ants.");
+  }else{
+    System.out.println("There are not 4 ants.")
+Scanner scan = new Scanner (System.in);
+String name = scan.next():
